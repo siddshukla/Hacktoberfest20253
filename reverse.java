79a0576aa741cf127e362a2a5e6d0be762782875
siddharth shukla <124035361+siddshukla@users.noreply.github.com>
2025-10-22 13:20:11 +0530

    Implement digit reversal in reverse.java

---
+++ reverse.java
@@ -0,0 +1,16 @@
+
+class reverse {
+    static int reverseDigits(int n) {
+        int revNum = 0;
+        while (n > 0) {
+            revNum = revNum * 10 + n % 10;
+            n = n / 10;
+        }
+        return revNum;
+    }
+  
+    public static void main(String[] args) {
+        int num = 4562;
+        System.out.println(reverseDigits(num));
+    }
+}
